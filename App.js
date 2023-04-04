82e664f073e9306830517b3e64b0edef61bc1d87
Tammi Nyberg <tamminyberg@gmail.com>
2023-04-04 10:02:19 +0200

    added stack navigation, 2 screens etc.

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Button, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+//Navigation fra React Navigation dokumentation - fungerer
+function DetailsScreen({navigation}){
+  return(
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}} >
+      <Text>Details screen</Text>
+      <Button title='Gå til flere detaljer'
+      onPress={()=> navigation.push('Details') } >
+      </Button>
+      <Button title='Gå til hjem'
+      onPress={() => navigation.navigate('Hjem')} >
+
+      </Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function HjemScreen({navigation}){
+  return(
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}} >
+<Text>Hjemme skærm</Text>
+<Button title='Gå til opgave'
+onPress={() => navigation.navigate('Details')} >
+</Button>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+
+export default function App() {
+  return (
+<NavigationContainer>
+  <Stack.Navigator initialRouteName='Hjem'>
+    <Stack.Screen name='Hjem' component={HjemScreen} />
+    <Stack.Screen name='Details' component={DetailsScreen}/>
+  </Stack.Navigator>
+</NavigationContainer>
+  );
+}
+//Navigation fra React Native dokumentation - fungerer 
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//         name='Home'
+//         component={HomeScreen}
+//         options={{title: 'Velkommen'}}
+//         />
+//         <Stack.Screen
+//         name="Assignment"
+//         component={AssignmentScreen}
+//         />
+//     </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
+// const HomeScreen =({navigation})=> {
+//   return (
+//     <Button
+//     title='Go to assignment'
+//     onPress={() =>
+//       navigation.navigate('Assignment', {name: 'assignment'})
+//     }
+//     />
+//   )
+// }
+// const AssignmentScreen=({navigation, route}) => {
+//   return <Text>This is {route.params.name} screen</Text>
+// }
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
